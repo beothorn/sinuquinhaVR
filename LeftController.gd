cff68931a6a7d9aae93caaa7a271909d2187bb6b
Beothorn <lucass@gmail.com>
2021-09-26 23:57:45 +0200

    Replace all

--- LeftController.gd
+++ LeftController.gd
@@ -1,8 +1,18 @@
 extends ARVRController
 
+const debounce_rotation = 0.33
+var deboucing_time = debounce_rotation
 
 func _physics_process(delta):
 	var y = get_joystick_axis(1) * delta
 	var x = -get_joystick_axis(0) * delta
 	get_parent().translate(Vector3(0,y,0))
-	get_parent().rotate_y(x)
+	if(deboucing_time < debounce_rotation):
+		deboucing_time += delta
+		return
+	if(x > 0.005):
+		get_parent().rotate_y(0.52)
+		deboucing_time = 0
+	if(x < -0.005):
+		get_parent().rotate_y(-0.52)
+		deboucing_time = 0
