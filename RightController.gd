9dfa0dc7abb5739ffe4be54f4967c93e7aa20778
Beothorn <lucass@gmail.com>
2021-10-01 01:04:23 +0200

    Now with cue stick

--- RightController.gd
+++ RightController.gd
@@ -3,16 +3,36 @@ extends ARVRController
 const CONTROLLER_DEADZONE = 0.65
 const MOVEMENT_SPEED = 1.5
 
+const x_axis = 0
+const y_axis = 1
+const trigger = 2
+
+var aiming_mode = false
+var cue_stick_pos
+var cue_stick_axis
+
 var cam: ARVRCamera
 
 func _ready():
 	cam = get_parent().get_node("ARVRCamera")
 
 func _physics_process(delta):
-	_physics_process_directional_movement(delta)
+	var was_aiming_mode = aiming_mode
+	aiming_mode = get_joystick_axis(trigger) > CONTROLLER_DEADZONE
+	$cueStick.visible = aiming_mode
+	if aiming_mode:
+		if not was_aiming_mode:
+			cue_stick_pos = $cueStick.translation
+			cue_stick_axis = $cueStick.transform.basis.y.normalized()
+		
+		$cueStick.translation = cue_stick_pos + (cue_stick_axis * get_joystick_axis(y_axis))
+	else:
+		if was_aiming_mode:
+			$cueStick.translation = cue_stick_pos
+		_physics_process_directional_movement(delta)
 
 func _physics_process_directional_movement(delta):
-	var joystick_vector = Vector2(-get_joystick_axis(1), get_joystick_axis(0))
+	var joystick_vector = Vector2(-get_joystick_axis(y_axis), get_joystick_axis(x_axis))
 
 	if joystick_vector.length() < CONTROLLER_DEADZONE:
 		joystick_vector = Vector2(0,0)
