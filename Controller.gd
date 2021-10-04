bf7d2f6e95cc9113effa81668233508070eda8f7
Beothorn <lucass@gmail.com>
2021-10-05 00:53:24 +0200

    Make left joystick go sideways instead of rotating

---
+++ Controller.gd
@@ -0,0 +1,37 @@
+extends ARVRController
+
+class_name QuestContorller
+
+const CONTROLLER_VECTOR_DEADZONE = 0.65
+const CONTROLLER_AXIS_DEADZONE = 0.3
+const TRIGGER_AXIS_DEADZONE = 0.2
+const X_AXIS = 0
+const Y_AXIS = 1
+const TRIGGER = 2
+
+func is_trigger_pressed() -> bool:
+	return get_joystick_axis(TRIGGER) > TRIGGER_AXIS_DEADZONE
+
+func get_x_axis(sensitivity = CONTROLLER_AXIS_DEADZONE) -> float:
+	return  _get_axis(X_AXIS, sensitivity)
+
+func get_y_axis(sensitivity = CONTROLLER_AXIS_DEADZONE) -> float:
+	return  _get_axis(Y_AXIS, sensitivity)
+
+func get_controller_vector() -> Vector2:
+	return Vector2(get_x_axis(), get_y_axis())
+
+func get_movement_vector() -> Vector2:
+	var controller_vector: Vector2 = Vector2(-get_joystick_axis(Y_AXIS), get_joystick_axis(X_AXIS))
+
+	if controller_vector.length() < CONTROLLER_VECTOR_DEADZONE:
+		controller_vector = Vector2(0,0)
+	else:
+		controller_vector = controller_vector.normalized() * ((controller_vector.length() - CONTROLLER_VECTOR_DEADZONE) / (1 - CONTROLLER_VECTOR_DEADZONE))
+	return controller_vector.normalized()
+
+func _get_axis(axis, sensitivity):
+	var raw_axis = get_joystick_axis(axis)
+	if raw_axis < sensitivity and raw_axis > -sensitivity:
+		return 0.0
+	return raw_axis
---
+++ Game.gd
@@ -0,0 +1,84 @@
+extends Spatial
+
+class_name Game
+
+
+const DEBOUNCE_ROTATION_TIME = 0.33
+const PLAYER_ROTATION_ANGLE = 0.53 # App. 30'
+const MOVEMENT_SPEED = 1.5
+
+onready var vr_player = $VRPlayer
+onready var vr_camera = $VRPlayer/VRCamera
+onready var left_controller: QuestContorller = $VRPlayer/LeftController
+onready var right_controller: QuestContorller = $VRPlayer/RightController
+onready var cueStick: RigidBody = $CueStick
+onready var displayCueStick: Spatial = $VRPlayer/RightController/CueStickModel
+onready var displayCueStickHitterPositon: CSGCylinder = $VRPlayer/RightController/hitterPosition
+
+var aiming_mode = false
+var deboucing_rotation_time_counter = DEBOUNCE_ROTATION_TIME
+
+func _ready():
+	_initialize_vr()
+
+func _initialize_vr():
+	var ovr_init_config_pre = preload("res://addons/godot_ovrmobile/OvrInitConfig.gdns")
+	var ovr_performance_pre = preload("res://addons/godot_ovrmobile/OvrPerformance.gdns")
+	
+	var ovr_init_config = ovr_init_config_pre.new()
+	var ovr_performance = ovr_performance_pre.new()
+	var interface = ARVRServer.find_interface("OVRMobile")
+	if interface:
+		ovr_init_config.set_render_target_size_multiplier(1)
+		if interface.initialize():
+			get_viewport().arvr = true
+
+func _physics_process(delta: float):
+	_process_left_controller_input(delta)
+	_process_right_controller_input(delta)
+
+func _process_left_controller_input(delta: float):
+	_move_player(delta, left_controller.get_movement_vector())
+	
+func _process_right_controller_input(delta: float):
+	var was_aiming_mode = aiming_mode
+	aiming_mode =  right_controller.is_trigger_pressed() 
+	cueStick.visible = aiming_mode
+	displayCueStick.visible = ! aiming_mode
+	var current_y_axis = right_controller.get_y_axis()
+	if aiming_mode and not was_aiming_mode:
+		_move_real_stick_to_joystick_pos()
+	if not aiming_mode and was_aiming_mode:
+		cueStick.sleeping = true
+	
+	if aiming_mode:
+		cueStick.apply_central_impulse(cueStick.transform.basis.y.normalized() * current_y_axis )
+		
+	if(deboucing_rotation_time_counter < DEBOUNCE_ROTATION_TIME):
+		deboucing_rotation_time_counter += delta
+	else:
+		var x = right_controller.get_x_axis()
+		
+		if(x > 0.0):
+			vr_player.rotate_y(-PLAYER_ROTATION_ANGLE)
+			deboucing_rotation_time_counter = 0
+			
+		if(x < 0.0):
+			vr_player.rotate_y(PLAYER_ROTATION_ANGLE)
+			deboucing_rotation_time_counter = 0
+
+func _move_player(delta: float, movement_vector: Vector2):
+	var forward_direction = vr_camera.global_transform.basis.z.normalized()
+	var right_direction = vr_camera.global_transform.basis.x.normalized()
+
+	var movement_forward = forward_direction * movement_vector.x * delta * MOVEMENT_SPEED
+	var movement_right = right_direction * movement_vector.y * delta * MOVEMENT_SPEED
+
+	movement_forward.y = 0
+	movement_right.y = 0
+
+	if movement_right.length() > 0 or movement_forward.length() > 0:
+		vr_player.global_translate(movement_right + movement_forward)
+
+func _move_real_stick_to_joystick_pos():
+	cueStick.global_transform = displayCueStickHitterPositon.global_transform
